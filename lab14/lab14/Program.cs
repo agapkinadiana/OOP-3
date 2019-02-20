95736766e53a6d59f6203ed9f1e35e5fed092c8a
Diana Agapkina <43079357+agapkinadiana@users.noreply.github.com>
2019-02-20 17:25:40 +0300

    Add files via upload

---
+++ lab14/lab14/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace lab14
+{
+    class Program
+    {
+        private static EventWaitHandle waitHandle = new ManualResetEvent(initialState: true);
+
+        static void Main(string[] args)
+        {
+            int n;
+            while (true)
+            {
+                Write("Write some number: ");
+                if (int.TryParse(ReadLine(), out n))
+                    break;
+                else
+                    WriteLine("Try again");
+            }
+            Thread secondThread = new Thread(WriteNumbersEverywhere);
+            secondThread.Name = "SecondThread";
+
+            methods.WriteProcessToFile(@"processInfo.txt");
+            methods.CurrentDomainInfo();
+
+            secondThread.Start(n);
+            methods.NewDomainActions();
+            // Stoped thread
+            waitHandle.Reset();
+
+            WriteLine("Thread status: " + secondThread.ThreadState.ToString());
+            WriteLine("Thread Name: " + secondThread.Name.ToString());
+            WriteLine("Thread id: " + secondThread.ManagedThreadId.ToString());
+
+            for (int i = 0; i < 100; i++)
+                WriteLine("Thread paused " + i);
+            // Resume thread
+            waitHandle.Set();
+
+            if (secondThread.IsAlive)
+            {
+                WriteLine("\n\nThread status: " + secondThread.ThreadState.ToString());
+                WriteLine("Thread Name: " + secondThread.Name.ToString());
+                WriteLine("Thread id: " + secondThread.ManagedThreadId.ToString());
+            }
+            else
+                WriteLine("Thread is dead\n\n");
+            WriteLine("====================================================");
+
+            Thread oddThread = new Thread(methods.OddAndEvenNumbersToConsole);
+            oddThread.Name = "oddThread";
+            Thread evenThread = new Thread(methods.OddAndEvenNumbersToConsole);
+            evenThread.Name = "evenThread";
+            evenThread.Priority = ThreadPriority.Highest;
+
+            // true = odd numbers, false = even numbers
+            ValueTuple<int, bool> oddnum = (n, true);
+            ValueTuple<int, bool> evennum = (n, false);
+
+            oddThread.Start(oddnum);
+            evenThread.Start(evennum);
+
+            // Waiting for end of processes
+            Thread.Sleep(2000);
+            methods.OddAndEvenNumbersToFile();
+            WriteLine("====================================================");
+
+            TimerCallback tm = new TimerCallback(RandomTimerMethod);
+            Timer timer = new Timer(tm, 10, 0, 2000);
+
+            ReadLine();
+            WriteLine("====================================================");
+
+            // Coming soon
+        }
+
+        static void WriteNumbersEverywhere(object num)
+        {
+            int n = (int)num;
+            using (StreamWriter sw = new StreamWriter(@"Numbers.txt", false, Encoding.Default))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    WriteLine("Second thread: " + i);
+                    sw.WriteLine("Second thread: " + i);
+                    Thread.Sleep(0);
+                    waitHandle.WaitOne();
+                }
+            }
+        }
+
+        static void RandomTimerMethod(object count)
+        {
+            int n = (int)count;
+            for (int i = 1; i < 9; i++, n++)
+                WriteLine("Timer " + (n * i).ToString());
+        }
+    }
+}
---
+++ lab14/lab14/methods.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using static System.Console;
+using System.Reflection;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lab14
+{
+    class methods
+    {
+        static object locker = new object();
+        static string oddNum;
+        static string evenNum;
+
+        public static void OddAndEvenNumbersToConsole(object num)
+        {
+            // true = odd numbers, false = even numbers
+            //lock (locker)
+            //{
+                var n = (ValueTuple<int, bool>)num;
+                for (int i = 0; i < n.Item1; i++)
+                {
+                    if (i % 2 == 1 && n.Item2)
+                    {
+                        string str = "Odd Thread: " + i + "\n";
+                        Write(str);
+                        oddNum += str;
+                      //  Thread.Sleep(100);
+                    }
+                    if (i % 2 == 0 && !n.Item2)
+                    {
+                        string str = "Even Thread: " + i + "\n";
+                        Write(str);
+                        evenNum += str;
+                        Thread.Sleep(0);
+                       // Thread.Sleep(100);
+                    }
+                }
+            //}
+        }
+
+        public static void OddAndEvenNumbersToFile()
+        {
+            using (StreamWriter sw = new StreamWriter(@"OddEvenNumbers.txt", false, Encoding.Default))
+            {
+                sw.WriteLine(oddNum);
+                sw.WriteLine(evenNum);
+            }
+        }
+
+
+        public static void WriteProcessToFile(string path)
+        {
+            var process = Process.GetProcesses();
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.Default))
+            {
+                foreach (var p in process)
+                {
+                    sw.WriteLine("ProcName: " + p.ProcessName);
+                    sw.WriteLine("ProcId: " + p.Id);
+                    //sw.WriteLine("ProcPriority: " + p.BasePriority);
+                    //sw.WriteLine("ProcStartTime: " + p.StartTime.ToString());
+                    //sw.WriteLine("ProcWorkingTime: " + p.TotalProcessorTime);
+                    sw.WriteLine("\n\n");
+                }
+            }
+            WriteLine("First task has complete\n\n");
+        }
+
+
+        public static void CurrentDomainInfo()
+        {
+            AppDomain domain = AppDomain.CurrentDomain;
+            WriteLine("Domain id: " + domain.Id);
+            WriteLine("Domain name: " + domain.FriendlyName);
+            WriteLine("Domain base directory: " + domain.BaseDirectory);
+            Assembly[] assemblies = domain.GetAssemblies();
+            WriteLine("Assemblies:");
+            foreach (var ass in assemblies)
+                WriteLine("Name: " + ass.GetName().Name);
+        }
+
+        public static void NewDomainActions()
+        {
+            AppDomain domain = AppDomain.CreateDomain("My custom domain");
+            domain.AssemblyLoad += DomainAssLoadEvent;
+            domain.DomainUnload += DomainUnloadEvent;
+
+            // Need to copy it to /bin/debug/
+            domain.Load(new AssemblyName("System.Data"));
+            Assembly[] assemblies = domain.GetAssemblies();
+            foreach (var ass in assemblies)
+            {
+                WriteLine("Name: " + ass.GetName().Name);
+                Thread.Sleep(50);
+            }
+
+            AppDomain.Unload(domain);
+        }
+        private static void DomainUnloadEvent(object sender, EventArgs e) => WriteLine("Домен выгружен из процесса");
+        private static void DomainAssLoadEvent(object sender, AssemblyLoadEventArgs args) => WriteLine("Сборка загружена");
+
+    }
+}
+
